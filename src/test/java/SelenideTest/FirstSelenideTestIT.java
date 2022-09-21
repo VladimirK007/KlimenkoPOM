dc456fedf7dd9d7177ccb750bfcdbe16e2086b61
VladimirK007 <vladimir.k.qarea@gmail.com>
2022-09-21 10:52:17 +0200

    Develop fixes 3

--- src/test/java/SelenideTest/FirstSelenideTestIT.java
+++ src/test/java/SelenideTest/FirstSelenideTestIT.java
@@ -20,11 +20,16 @@ public class FirstSelenideTestIT {
         $(By.xpath("//input[@id='product_attribute_3_6']")).click();
 
         $(By.xpath("//input[@id='product_attribute_4_8']")).click();
+
         $(By.xpath("//button[@id='add-to-cart-button-1']")).click();
+
         $(By.xpath("//p[@class='content']" +
                 "[contains(.,'The product has been added to your ')]")).should(disappear);
+
         $(By.xpath("//span[@class='cart-label']")).click();
+
         $(By.xpath("//td[@class='product']//div[@class='attributes'][contains(.,'RAM: 8GB')]")).shouldBe(visible);
+
         $(By.xpath("//td[@class='product']//div[@class='attributes'][contains(.,'HDD: 320 GB')]")).shouldBe(visible);
 
     }
